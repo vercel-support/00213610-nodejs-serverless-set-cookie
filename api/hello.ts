8b2177245709ff988affbe9c027db8edfaa5f049
Nick Vigier <nvigier46@gmail.com>
2024-03-28 09:22:29 +0000

    testing edge

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { type VercelRequest, VercelResponse } from '@vercel/node';
+
+export const config = {
+  runtime: 'edge',
+};
 
 // export default function handler(req: VercelRequest, res: VercelResponse) {
 //   const { name = 'World' } = req.query
@@ -7,12 +11,20 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 //   })
 // }
 
-export async function GET(req: Request) {
-  const res = new Response('Test append cookies', {
-    headers: {
-      'content-type': 'text/html',
-    },
-  });
+export async function GET(req: VercelRequest) {
+  // const res = new Response('Test append cookies', {
+  //   headers: {
+  //     'content-type': 'text/html',
+  //   },
+  // });
+  const res = new Response();
+  console.log({ res });
+
+  // res.setHeader('set-cookie', [
+  //   'cookie_1=value',
+  //   'cookie_2=value',
+  //   'cookie_3=value',
+  // ]);
   res.headers.set('set-cookie', 'cookie_1=value');
   res.headers.append('set-cookie', 'cookie_2=value');
   res.headers.append('set-cookie', 'cookie_3=value');
