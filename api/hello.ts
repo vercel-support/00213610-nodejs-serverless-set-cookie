fa8c2347b079c22c28a70ead752f4f2f65c17adc
Nick Vigier <nvigier46@gmail.com>
2024-03-28 08:58:13 +0000

    Test

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,20 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   const { name = 'World' } = req.query
+//   return res.json({
+//     message: `Hello ${name}!`,
+//   })
+// }
+
+export async function GET(req: Request) {
+  const res = new Response('Test append cookies', {
+    headers: {
+      'content-type': 'text/html',
+    },
+  });
+  res.headers.set('set-cookie', 'cookie_1=value');
+  res.headers.append('set-cookie', 'cookie_2=value');
+  res.headers.append('set-cookie', 'cookie_3=value');
+  return res;
 }
